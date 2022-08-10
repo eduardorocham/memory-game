57346c4fa1b5d3a223d3e03e5ad2a496fc0bbe1b
Eduardo Rocha <97197375+eduardorocham@users.noreply.github.com>
2022-08-10 18:36:37 -0300

    grid created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import * as C from './app.styles';
 import logoImage from './assets/devmemory_logo.png';
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
+import RestartIcon from './svgs/restart.svg';
+
 
 const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+
+  const resetAndCreateGrid = () => {
+
+  }
+
+  useEffect(() => resetAndCreateGrid, []);
+
   return (
     <div>
       <C.Container>
@@ -11,13 +24,14 @@ const App = () => {
           </C.LogoLink>
 
           <C.InfoArea>
-
+            <InfoItem label='Tempo' value='00:00'></InfoItem>
+            <InfoItem label='Movimentos' value='0'></InfoItem>
           </C.InfoArea>
 
-          <button>Reiniciar</button>
+          <Button label='Resetar' icon={RestartIcon} onClick={resetAndCreateGrid}></Button>
         </C.Info>
         <C.GridArea>
-
+          <C.Grid></C.Grid>
         </C.GridArea>
       </C.Container>
     </div>
---
+++ src/data/items.ts
@@ -0,0 +1,15 @@
+import android from '../svgs/android.svg';
+import caminhao from '../svgs/caminhao.svg';
+import disney from '../svgs/disney.svg';
+import estrela from '../svgs/estrela.svg';
+import gasolina from '../svgs/gasolina.svg';
+import moto from '../svgs/moto.svg';
+
+export const items = [
+    {name: 'android', icon: android},
+    {name: 'caminhao', icon: caminhao},
+    {name: 'disney', icon: disney},
+    {name: 'estrela', icon: android},
+    {name: 'android', icon: estrela},
+    {name: 'moto', icon: moto}
+]
